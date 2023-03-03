96ac517d0afa15ac8cf5faddc7abe7a81563baff
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2023-03-02 18:25:21 -0800

    Add CSS loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,10 +20,22 @@ module.exports = () => {
     plugins: [
       
     ],
-
     module: {
       rules: [
-        
+        {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader:'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        }
+      }
       ],
     },
   };
